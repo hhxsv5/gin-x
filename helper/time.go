93b195e17b2f14a932e6d6991abb23088ce0a7d0
XieBiao <hhxsv5@sina.com>
2018-04-10 11:20:07 +0800

    more time helper

--- helper/time.go
+++ helper/time.go
@@ -2,6 +2,11 @@ package helper
 
 import "time"
 
+var (
+	TimeLayoutDate     = "2006-01-02"
+	TimeLayoutDateTime = "2006-01-02 15:04:05"
+)
+
 func TodayStart() time.Time {
 	y, m, d := time.Now().Date()
 	return time.Date(y, m, d, 0, 0, 0, 0, time.Local)
@@ -10,4 +15,32 @@ func TodayStart() time.Time {
 func TodayEnd() time.Time {
 	y, m, d := time.Now().Date()
 	return time.Date(y, m, d, 23, 59, 59, 1E9-1, time.Local)
+}
+
+func NowUnix() int64 {
+	return time.Now().Unix()
+}
+
+func NowDate() string {
+	return time.Now().Format(TimeLayoutDate)
+}
+
+func NowDateTime() string {
+	return time.Now().Format(TimeLayoutDateTime)
+}
+
+func ParseDate(dt string) (time.Time, error) {
+	return time.Parse(TimeLayoutDate, dt)
+}
+
+func ParseDateTime(dt string) (time.Time, error) {
+	return time.Parse(TimeLayoutDateTime, dt)
+}
+
+func ParseStringTime(tm, lc string) (time.Time, error) {
+	loc, err := time.LoadLocation(lc)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return time.ParseInLocation(TimeLayoutDateTime, tm, loc)
 }
