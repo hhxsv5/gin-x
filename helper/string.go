fd3bc2c149fed6876fa333fece0ee5067f4154b5
XieBiao <hhxsv5@sina.com>
2018-04-10 11:16:44 +0800

    str conv

--- helper/string.go
+++ helper/string.go
@@ -3,12 +3,29 @@ package helper
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"strconv"
 )
 
 func Md5(s string) string {
 	ctx := md5.New()
 	ctx.Write([]byte(s))
 	return hex.EncodeToString(ctx.Sum(nil))
+}
+
+func StringToInt64(s string) int64 {
+	r, e := strconv.ParseInt(s, 10, 64)
+	if e != nil {
+		return 0
+	}
+	return r
+}
+
+func StringToFloat64(s string) float64 {
+	r, e := strconv.ParseFloat(s, 64)
+	if e != nil {
+		return 0
+	}
+	return r
 }
 
 func StringSliceToStringMap(input []string) map[string]string {
