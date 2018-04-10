be53ef7fa81435e65bde455131e42e738d280090
XieBiao <hhxsv5@sina.com>
2018-04-10 11:08:09 +0800

    string slice helper

--- helper/string.go
+++ helper/string.go
@@ -10,3 +10,20 @@ func Md5(s string) string {
 	ctx.Write([]byte(s))
 	return hex.EncodeToString(ctx.Sum(nil))
 }
+
+func StringSliceToStringMap(input []string) map[string]string {
+	r := map[string]string{}
+	for _, s := range input {
+		r[s] = s
+	}
+	return r
+}
+
+func StringSliceUnique(input []string) []string {
+	r := StringSliceToStringMap(input)
+	nr := make([]string, 0, len(r))
+	for key, _ := range r {
+		nr = append(nr, key)
+	}
+	return nr
+}
